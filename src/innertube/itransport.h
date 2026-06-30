5273d2c2feea572b4e2dbc83805b3918ae97b4d4
IarChep <yarchepel@gmail.com>
2026-06-30 12:51:40 +0400

    feat(innertube): port ITransport + InnertubeClient (owner-tied callbacks + timeout)

--- src/innertube/itransport.h
+++ src/innertube/itransport.h
@@ -4,14 +4,17 @@
 #include <QString>
 #include <nlohmann/json.hpp>
 #include "clientconfig.h"
+class QObject;
 namespace yt {
 struct Reply { bool ok; nlohmann::json json; QString error; Reply() : ok(false) {} };
 typedef std::function<void(const Reply &)> ReplyFn;
 class ITransport {
 public:
     virtual ~ITransport() {}
-    virtual void post(const QString &endpoint, ClientId client, const nlohmann::json &body, ReplyFn cb) = 0;
-    virtual void get(const QString &url, ReplyFn cb) = 0;
+    // owner (optional): if given and it is destroyed before the request finishes,
+    // the callback is dropped and the in-flight reply is aborted (use-after-free fix).
+    virtual void post(const QString &endpoint, ClientId client, const nlohmann::json &body, ReplyFn cb, QObject *owner = 0) = 0;
+    virtual void get(const QString &url, ReplyFn cb, QObject *owner = 0) = 0;
 };
 }
 #endif
