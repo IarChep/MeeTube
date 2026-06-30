2d83cd383d370c7adf91dfebb7dd41bbd9591b4b
IarChep <yarchepel@gmail.com>
2026-06-30 15:43:35 +0400

    feat(app): adopt Harmattan QmlApplicationViewer + MDeclarativeCache booster (from MeeShopGUI)

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
 #include <QApplication>
-#include <QDeclarativeView>
-#include <QtDeclarative>
+#include <QScopedPointer>
+#include <QTextCodec>
+
+#include "qmlapplicationviewer/qmlapplicationviewer.h"
+#include <QtDeclarative/qdeclarative.h>
 #include <QDeclarativeContext>
+#include <QDeclarativeEngine>
+#include <QDeclarativeView>
+
 #include "models/videomodel.h"
 #include "models/servicemetatypes.h"
 #include "innertube/innertube.h"
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+Q_DECL_EXPORT int main(int argc, char *argv[])
+{
+    QScopedPointer<QApplication> app(createApplication(argc, argv));
+    QCoreApplication::setApplicationName("MeeTube");
+    QCoreApplication::setOrganizationName("MeeTube");
+
+    QTextCodec *utfCodec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(utfCodec);
+    QTextCodec::setCodecForCStrings(utfCodec);
+    QTextCodec::setCodecForTr(utfCodec);
+
 #ifdef WEBP_PLUGIN_DIR
-    app.addLibraryPath(QLatin1String(WEBP_PLUGIN_DIR));
+    app->addLibraryPath(QLatin1String(WEBP_PLUGIN_DIR));
 #endif
-    app.setOrganizationName("MeeTube");
-    app.setApplicationName("MeeTube");
 
     registerMeeTubeMetaTypes();
     qmlRegisterType<VideoModel>("MeeTube", 1, 0, "VideoModel");
 
-    QDeclarativeView view;
-    view.rootContext()->setContextProperty("innertube", yt::Innertube::instance());
-    view.setSource(QUrl("qrc:/qml/main.qml"));   // UI is out of scope; placeholder for now
-    return app.exec();
+    QmlApplicationViewer viewer;
+    viewer.rootContext()->setContextProperty("innertube", yt::Innertube::instance());
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockPortrait);
+    viewer.setSource(QUrl("qrc:/qml/main.qml"));   // UI is out of scope; placeholder for now
+    viewer.showExpanded();
+
+    return app->exec();
 }
